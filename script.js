a749ad362ed1d3226e03076b94c04e8b9c92fb40
Navya Kashyap <knavya929@gmail.com>
2026-08-27 11:08:16 +0530

    modify

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const navbarContainer = document.getElementById("navbar");
 
                 <!-- Athenura Logo -->
                 <a href="index.html" class="navbar-logo">
-                    <img src="images/logo/athenura-logo.png" alt="Athenura">
+                    <img src="Resources/images/logo/athenura-logo.png" alt="Athenura">
                 </a>
 
 
